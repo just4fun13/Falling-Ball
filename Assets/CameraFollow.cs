bb3d999f7e1a671a7584ecd0d3f33e9a6ab8aa85
just4fun13 <66634008+just4fun13@users.noreply.github.com>
2020-07-28 19:54:26 +0900

    Add files via upload

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    Vector3 startPosition = Vector3.zero;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + startPosition;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 startPosition = Vector2.zero;
+    Vector2 position = Vector2.zero;
+    [SerializeField]
+    Transform morphT;
+    [SerializeField]
+    Transform morphT2;
+    
+    Transform curTransform;
+    // Start is called before the first frame update
+    void Start()
+    {       
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        curTransform = morphT;
+    }
+
+    public void OnPointerDown (BaseEventData data)
+    {
+        PointerEventData pointer = (PointerEventData) data;
+        startPosition = pointer.position;
+    }
+
+    public void OnDrag ( BaseEventData data)
+    {
+        PointerEventData pointer = (PointerEventData) data;
+        position = pointer.position - startPosition;
+//        morphT.rotation = Quaternion.Euler (position.y/10f, 0f, -position.x/10f);
+        curTransform.Rotate (Time.deltaTime * 0.1f * new Vector3(position.x , -position.y, 0f));
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform != curTransform)
+        {
+            if (curTransform == morphT)
+            {
+                morphT.rotation = Quaternion.identity;
+                morphT.position = new Vector3 (morphT2.position.x, morphT2.position.y, morphT2.position.z - 17f);
+            }
+            else
+            {
+                morphT2.rotation = Quaternion.identity;
+                morphT2.position = new Vector3 (morphT.position.x, morphT.position.y + 17f, morphT.position.z - 17f); 
+            }
+            curTransform = collision.transform;
+            Debug.Log ("Time has changed");
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
